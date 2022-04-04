0cf1c3ed0c15c98d8251312e96755e3938808302
atarannn <anastasiatarannn@icloud.com>
2022-04-05 01:32:56 +0300

    added privacy page layout and some layout for home page adaptive layout

--- src/assets/scripts/gulp-modules/home.js
+++ src/assets/scripts/gulp-modules/home.js
@@ -3,7 +3,18 @@ $('.fag-card-text').on('click', function(e){
   $(this).next('.section-8 .regular-text').slideToggle();
 });
 
-const slider = new Swiper('.swiper', {
+const slider = new Swiper('.swiper-1', {
+  loop: false,
+  speed: 1000,
+  spaceBetween: 20,
+  width: 150,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
+
+const slider2 = new Swiper('.swiper-2', {
   loop: false,
   speed: 1000,
   spaceBetween: 20,
