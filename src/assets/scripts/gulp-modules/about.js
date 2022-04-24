eb20579d0bfe0365a12f2109c6d944823eb758a1
atarannn <anastasiatarannn@icloud.com>
2022-04-25 00:58:51 +0300

    added pages layout

---
+++ src/assets/scripts/gulp-modules/about.js
@@ -0,0 +1,10 @@
+const slider = new Swiper('.swiper-about', {
+  loop: false,
+  speed: 1000,
+  spaceBetween: 20,
+  width: 130,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
